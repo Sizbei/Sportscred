172c0421f1ab776534ddc877b880473eb712df67
1DWalker <alex.j.sheng.yang@gmail.com>
2020-10-13 16:25:27 -0400

    [29] Fix Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from "./Navbar"
+// import Navbar from "./Navbar"
 import logo from '../res/whitesportcred.png';
 import logoborder from '../res/ACSBorder.png';
 import placeholderprofile from '../res/dog.jpg';
@@ -8,13 +8,35 @@ import placeholderprofile from '../res/dog.jpg';
 import "./Header.css"
 
 export default function Header() {
-  return (
+  return (    
     <div className="header">
     <Link to="/" className="navbar-brand"><img src={logo} className="logo" alt="SportCred" href="the_zone"/></Link>
     {/* <HeaderMenu /> */}
     <Navbar />
     <User />
     </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <ul className="navbar">
+      <li className="navbar-item">
+      <Link to="/" className="nav-link">The Zone</Link>
+      </li>
+      <li className="navbar-item">
+      <Link to="/trivia" className="nav-link">Trivia</Link>
+      </li>
+      <li className="navbar-item">
+      <Link to="/analysis" className="nav-link">Analysis</Link>
+      </li>
+      <li className="navbar-item">
+      <Link to="/picksandpredictions" className="nav-link">Picks/Predictions</Link>
+      </li>
+      <li className="navbar-item">
+      <Link to="/profile" className="nav-link">Profile</Link>
+      </li>
+    </ul>
   );
 }
 
@@ -30,3 +52,4 @@ function User() {
     </div>
   );
 }
+
