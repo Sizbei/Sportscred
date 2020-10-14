3b304e55d0f002138ccde3d779cbd17d38d8804d
1DWalker <alex.j.sheng.yang@gmail.com>
2020-10-14 17:19:02 -0400

    [13] Fix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../res/whitesportcred.png';
+import logo from '../res/SportCredLogo.png';
 
 import "./Header.css"
 
