be55e46bc5ddb77c17437b4f6cbd69dd26a95bc6
Hongfei Chen <hongfei.chen@mail.utoronto.ca>
2020-10-20 17:01:25 -0400

    fix small issues

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -1,36 +1,52 @@
+import axios from "axios";
+
 export default {
+    //POST request using axios
+    //login : user => {
+    //    const userBody = {
+    //        params: user
+    //    }
+    //    return axios.post('http://localhost:5000/login', {
+    //        withCredentials: true,
+    //        headers: {
+    //            'Content-Type' : 'application/json'
+    //        }
+    //    }, userBody)
+    //        .then(res => res.data);
+    //},
+    
     login : user => {
-        return fetch('http://localhost:5000/login', {
+        return fetch('/login', {
             method :  "post",
             body : JSON.stringify(user),
             headers: {
-                'Content-Type' : 'application/json'
+               'Content-Type' : 'application/json'
             }
         })
         .then(res => res.json())
         .then(data => data);
     },
 
-    signup : user => {
-        return fetch('http://localhost:5000/signup', {
-            method :  "post",
-            body : JSON.stringify(user),
-            headers: {
-                'Content-Type' : 'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(data => data);
-    },
+    //signup : user => {
+    //    return fetch('/signup', {
+    //        method :  "post",
+    //        body : JSON.stringify(user),
+    //        headers: {
+    //            'Content-Type' : 'application/json'
+    //        }
+    //    })
+    //    .then(res => res.json())
+    //    .then(data => data);
+    //},
 
     logout : () => {
-        return fetch('http://localhost:5000/logout')
+        return fetch('/logout')
             .then(res => res.json())
             .then(data => data);
     },
 
     isAuthenticated : () => {
-        return fetch('http://localhost:5000/login/authenticated')
+        return fetch('/login/authenticated')
             .then(res => {
                 // if passport send the 401 status, that means the user is not authorized
                 if(res.status !== 401) {
